1619fb81807ea0cfb02c68f748c57e81c2288cbe
rihan010 <ibrhimrihan000@gmail.comm>
2023-02-17 22:38:29 +0600

    design-complete

---
+++ js/utilities.js
@@ -0,0 +1,7 @@
+function pointFiveAreaCalculator(event) {
+    const num1 = event.target.parentNode.parentNode.children[2].children[0].value;
+    const num2 = event.target.parentNode.parentNode.children[2].children[2].value;
+    const areaPrevious = 0.5 * parseFloat(num1) * parseFloat(num2);
+    const area = areaPrevious.toFixed(2);
+    return area;
+}
