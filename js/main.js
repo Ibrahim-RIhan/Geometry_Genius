1245a0745aa76fbd9fa5ab27573ab9c37ce2a535
rihan010 <ibrhimrihan000@gmail.comm>
2023-02-17 23:22:02 +0600

    two function added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-document.getElementById('btn-triangle').addEventListener('click', function(event) {
-//     const num1 = event.target.parentNode.parentNode.children[2].children[0].value;
-//     const num2 = event.target.parentNode.parentNode.children[2].children[2].value;
-//     const areaPrevious =  0.5 * parseFloat(num1) * parseFloat(num2);
-// const area = areaPrevious.toFixed(2);
-// return area;
-const triangleArea = pointFiveAreaCalculator(event);
-console.log(triangleArea);
-
+const buttonTriangle = document.getElementById('btn-triangle').addEventListener('click', function (event) {
+    const element = elementFunction(event)
+    const area = pointFiveAreaCalculator(event)
+    displayData(element, area);
 })
+
+// const triangleArea = pointFiveAreaCalculator(event);
+// console.log(triangleArea);
--- js/utilities.js
+++ js/utilities.js
@@ -5,3 +5,17 @@ function pointFiveAreaCalculator(event) {
     const area = areaPrevious.toFixed(2);
     return area;
 }
+// Element Function 
+function elementFunction(event) {
+    const element = event.target.parentNode.parentNode.children[0].innerText;
+    return element;
+}
+function displayData(element, area) {
+    const areaContainer = document.getElementById('areaContainer');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+<td>${element}</td>
+<td>${area}</td>
+<button class="bg-blue-500 text-center mt-3 px-2 py-1 text-white rounded-md">ConvertToMeter</button>`;
+    areaContainer.appendChild(tr);
+}
