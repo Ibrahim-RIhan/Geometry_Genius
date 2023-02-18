00b5e0ca7cd8bb77a696e5fab532c996c47c838d
rihan010 <ibrhimrihan000@gmail.comm>
2023-02-18 08:51:17 +0600

    function validation added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,68 @@
-const buttonTriangle = document.getElementById('btn-triangle').addEventListener('click', function (event) {
-    const element = elementFunction(event)
-    const area = pointFiveAreaCalculator(event)
-    displayData(element, area);
+// Triangle Area 
+document.getElementById('btn-triangle').addEventListener('click', function (event) {
+    const area = pointFiveAreaCalculator(event);
+    if (area === undefined) {
+    }
+    else {
+        const element = elementFunction(event);
+        displayData(element, area);
+    }
 })
 
-// const triangleArea = pointFiveAreaCalculator(event);
-// console.log(triangleArea);
+// Rectangle Area 
+document.getElementById('btn-rectangle').addEventListener('click', function (event) {
+    const area = areaFunction(event);
+    if (area === undefined) {
+    }
+    else {
+        const element = elementFunction(event);
+        displayData(element, area);
+    }
+});
+
+// Parallelogram Area 
+document.getElementById('btn-parallelogram').addEventListener('click', function (event) {
+    const area = areaFunction(event);
+    if (area === undefined) {
+    }
+    else {
+        const element = elementFunction(event);
+        displayData(element, area);
+    }
+});
+
+// Rhombus Area 
+document.getElementById('btn-rhombus').addEventListener('click', function (event) {
+    const area = pointFiveAreaCalculator(event);
+    if (area === undefined) {
+    }
+    else{
+        const element = elementFunction(event);
+        displayData(element, area);
+    }
+   
+});
+// Pentagon Area 
+document.getElementById('btn-pentagon').addEventListener('click', function (event) {
+    
+    
+    const area = pointFiveAreaCalculator(event);
+    
+})
+//  Ellipse Area 
+
+document.getElementById('btn-ellipse').addEventListener('click', function (event) {
+    const num1 = event.target.parentNode.parentNode.children[2].children[0].value;
+    const num2 = event.target.parentNode.parentNode.children[2].children[2].value;
+    if (num1, num2 >= 0 && typeof num1, num2 === 'number') {
+        const areaPrevious = 3.14 * parseFloat(num1) * parseFloat(num2);
+        const area = areaPrevious.toFixed(0) + "m²";
+        const element = elementFunction(event);
+        displayData(element, area);
+    }
+    else {
+        alert("Please enter a valid Positive number");
+    }
+}
+
+)
--- js/utilities.js
+++ js/utilities.js
@@ -1,21 +1,53 @@
+// Point Five Multiply Area Function 
+
 function pointFiveAreaCalculator(event) {
-    const num1 = event.target.parentNode.parentNode.children[2].children[0].value;
-    const num2 = event.target.parentNode.parentNode.children[2].children[2].value;
-    const areaPrevious = 0.5 * parseFloat(num1) * parseFloat(num2);
-    const area = areaPrevious.toFixed(2);
-    return area;
+    const num1String = event.target.parentNode.parentNode.children[2].children[0].value;
+    const num2String = event.target.parentNode.parentNode.children[2].children[2].value;
+    const num1 = parseFloat(num1String);
+    const num2 = parseFloat(num2String);
+    if (num1 > 0 && num2 > 0 && typeof num1 === 'number' && typeof num2 === 'number') {
+        const areaPrevious = 0.5 * parseFloat(num1) * parseFloat(num2);
+        const area = areaPrevious.toFixed(0) + "m²";
+        return area;
+    }
+    else {
+        alert("Please enter a valid Positive number");
+    }
 }
+
+// Area Function 
+
+function areaFunction(event) {
+    const num1String = event.target.parentNode.parentNode.children[2].children[0].value;
+    const num2String = event.target.parentNode.parentNode.children[2].children[2].value;
+    const num1 = parseFloat(num1String);
+    const num2 = parseFloat(num2String);
+    if (num1 > 0 && num2 > 0 && typeof num1 === 'number' && typeof num2 === 'number') {
+        const areaPrevious = parseFloat(num1) * parseFloat(num2);
+        const area = areaPrevious.toFixed(0) + "m²";
+        return area;
+    }
+    else {
+        alert("Please enter a valid Positive number");
+    }
+}
+
+
 // Element Function 
 function elementFunction(event) {
     const element = event.target.parentNode.parentNode.children[0].innerText;
     return element;
 }
+
+
+
+// Display Data Function 
 function displayData(element, area) {
     const areaContainer = document.getElementById('areaContainer');
     const tr = document.createElement('tr');
     tr.innerHTML = `
 <td>${element}</td>
 <td>${area}</td>
-<button class="bg-blue-500 text-center mt-3 px-2 py-1 text-white rounded-md">ConvertToMeter</button>`;
+<button class="bg-blue-500 text-center text-xs mt-3 px-2 py-1 text-white rounded-md">ConvertToMeter</button>`;
     areaContainer.appendChild(tr);
 }
